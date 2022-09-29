6c9a134b3cc45bab0d8bf16b0f9bd6b714aa4b37
KingMo <macv123321@gmail.com>
2022-09-28 23:28:07 -0400

    messed with img css

--- collage/src/components/Main/Main.js
+++ collage/src/components/Main/Main.js
@@ -1,13 +1,28 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 import React from 'react'
+import "./main.css"
+import IMG2 from '/Users/moisescardenas/Desktop/MaCV/collage/src/assets/2022/IMG2.jpeg'
+
 
 // THIS SHOULD BE THE AREA IN MAIN PAGE FILLED WITH PHOTOS
 const Main = () => {
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-        <Typography>
-            HELLO WORLD
-        </Typography>
+    <Grid id="grid" container  justifyContent="center" alignItems="center">
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
+      <img src={IMG2} alt='Truck in field' height={200} width={200}/>
     </Grid>
   )
 }
